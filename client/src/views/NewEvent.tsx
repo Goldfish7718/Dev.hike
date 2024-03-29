bd3019b9d03cc9b70d70ad947fc402eede19f992
prachi93-web <mehetreprachi91@gmail.com>
2024-03-29 20:41:48 +0530

    fix[NewEvent]: Add input for organizer;

--- client/src/views/NewEvent.tsx
+++ client/src/views/NewEvent.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Textarea } from "@/components/ui/textarea"
-import { MapPin } from "lucide-react"
+import { CalendarPlus } from "lucide-react"
 
 const NewEvent = () => {
     return (
@@ -11,19 +11,27 @@ const NewEvent = () => {
                 <div className="w-full p-4 sm:w-3/4 md:1/2">
                     <h3 className="mb-4 text-center">New Event</h3>
                     <div className="mb-4 ">                            
-                        <Label className="text-base md:text-lg">Event Name :</Label>
+                        <Label className="text-base md:text-lg">Event Title :</Label>
                         <Input className="text-base md:text-lg" />
                     </div>
                     <div className="mb-4">
                         <Label className="text-base md:text-lg">Event Description :</Label>
                         <Textarea className="text-base md:text-lg" />
+                    </div>
+                    <div className="mb-4">
+                        <Label className="text-base md:text-lg">Event Organizer :</Label>
+                        <div className="flex gap-2">
+                            <Input className="text-base md:text-lg" />
+                            <Button variant='outline'>Use My Default Name</Button>
+                        </div>
+                        
                     </div>                        
                     <div className="mb-4">
-                        <Label className="flex items-center text-base md:text-lg">Event Location <MapPin className="mx-1" size={18} /></Label>
-                        <Input className="text-base md:text-lg" />
+                        <Label className="flex items-center text-base md:text-lg">Event Location :</Label>
+                        <Input className="text-base md:text-lg" />   
                     </div>
                     <div className="flex justify-end">
-                        <Button className="text-lg my-6 w-full">Add Event</Button>
+                        <Button className="text-lg my-6 w-full">Add Event<CalendarPlus className="mx-2" size={24} /></Button>
                     </div>
                </div>
             </div>
