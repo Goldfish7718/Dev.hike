6c46d6f06de5c948527354e8813d9ef6c585fa0a
prachi93-web <mehetreprachi91@gmail.com>
2024-04-05 19:48:39 +0530

    build[InitiateProfileSecond]: Add Initial Profile Second page;

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,45 +6,42 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 // Layout Imports
 import RootLayout from './layouts/root-layout'
-import DashboardLayout from './layouts/dashboard-layout.tsx'
-import ProtectRoute from './layouts/protect-route.tsx'
 
 // View Imports
 import SignInComponent from './views/SignIn'
 import SignUpComponent from './views/SignUp'
 import Dashboard from './views/Dashboard'
-import Landing from './views/Landing.tsx'
+import DashboardLayout from './layouts/dashboard-layout.tsx'
+import  GlobeDemo  from './views/Landing.tsx'
 import NewTimeLine from './views/NewTimeLine.tsx'
 import NewEvent from './views/NewEvent.tsx'
 import NewPost from './views/NewPost.tsx'
 import Profile from './views/Profile.tsx'
+import InitiateProfileSecond from './views/InitiateProfileSecond.tsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
     children: [
-      { path: '/', element: <Landing /> },
+      { path: '/', element: <GlobeDemo /> },
       { path: '/sign-in', element: <SignInComponent /> },
       { path: '/sign-up', element: <SignUpComponent /> },
-      { 
-        element: <ProtectRoute />,
+      { path: '/profile', element: <Profile /> },     
+      { path: '/new/timeline', element: <NewTimeLine /> },
+      { path: '/new/event', element: <NewEvent /> },
+      { path: '/new/post', element: <NewPost /> },
+      { path: '/initiate-profile/2', element: <InitiateProfileSecond /> },
+      {
+        path: '/dashboard',
+        element: <DashboardLayout />,
         children: [
-          { path: '/profile', element: <Profile /> },     
-          { path: '/new/timeline', element: <NewTimeLine /> },
-          { path: '/new/event', element: <NewEvent /> },
-          { path: '/new/post', element: <NewPost /> },
           {
-            element: <DashboardLayout />,
-            children: [
-              {
-                path: '/dashboard',
-                element: <Dashboard />
-              }
-            ]
+            path: '/dashboard',
+            element: <Dashboard />
           }
         ]
-      },
+      }
     ]
   }
 ])
---
+++ client/src/views/InitiateProfileSecond.tsx
@@ -0,0 +1,47 @@
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button"
+import { Separator } from "@/components/ui/separator";
+import { GithubIcon, InstagramIcon, LinkedinIcon, Plus, Twitter, Link} from "lucide-react"
+
+const InitiateProfileSecond = () => {
+  return (
+    <>
+        <div className="flex justify-center h-screen items-center">
+          <div className="w-full p-4 sm:w-3/4 md:w-1/2"> 
+            <h3 className="flex items-left justify-start mb-4 text-center gap-2"><Link className="mx-1 my-1" size={24}/> Connect Your Socials</h3> 
+            <div className="mb-4 flex flex-row gap-2">
+              <Label className="flex items-center text-base md:text-lg mb-1"><GithubIcon className="mx-1" size={24}/> :</Label>
+              <Input className="text-base md:text-lg"/>
+            </div>
+            <div className="mb-4 flex flex-row gap-2">
+              <Label className="flex items-center text-base md:text-lg"><Twitter className="mx-1" size={24}/> :</Label>
+              <Input className="text-base md:text-lg"/>
+            </div>
+            <div className="mb-4 flex flex-row gap-2">
+              <Label className="flex items-center text-base md:text-lg"><LinkedinIcon className="mx-1" size={24}/> :</Label>
+              <Input className="text-base md:text-lg"/>
+            </div>
+            <div className="mb-4 flex flex-row gap-2">
+              <Label className="flex items-center text-base md:text-lg"><InstagramIcon className="mx-1" size={24}/> :</Label>
+              <Input className="text-base md:text-lg"/>
+            </div>
+            <Separator />
+            <div className="flex items-center mt-4 mb-4 gap-4">
+              <Label className="text-base md:text-lg">Other :</Label>
+              <Button className="p-1 h-8 w-12"><Plus className="mx-1" size={24}/></Button>
+            </div>
+            <div className="mt-4">
+              <Button className="mt-4 md:text-lg w-full">Connect</Button>
+            </div>
+            <div className="mt-4 flex justify-between">
+              <Button className="mt-4 md:text-lg w-1/6">Previous</Button>
+              <Button className="mt-4 md:text-lg w-1/6">Let's Go</Button>
+            </div>
+          </div>
+        </div>
+    </>
+  )
+}
+
+export default InitiateProfileSecond;
