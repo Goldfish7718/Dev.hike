c031aca26494ae3da085edbbf21b31fbc55f771c
Samruddhi@2405 <samruddhinarkhede7293@gmail.com>
2024-04-14 20:43:09 +0530

    feat[Backend/Events]: Add Event controllers;

--- server/controllers/eventControllers.js
+++ server/controllers/eventControllers.js
@@ -1,6 +1,12 @@
+import Event from "../models/eventSchema.js";
+
 export const getEvents = async (req, res) => {
     try {
-        
+        const events = await Event.find({})
+
+        res
+            .status(200)
+            .json({ events })
     } catch (error) {
         console.log(error);
         res
@@ -11,7 +17,18 @@ export const getEvents = async (req, res) => {
 
 export const addEvent = async (req, res) => {
     try {
-        
+        const { title, description, organiser, location } = req.body 
+
+        const newEvent = await Event.create({
+            title,
+            description,
+            organiser,
+            location
+        })
+
+        res
+            .status(200)
+            .json({ newEvent }) 
     } catch (error) {
         console.log(error);
         res
@@ -22,7 +39,13 @@ export const addEvent = async (req, res) => {
 
 export const deleteEvent = async (req, res) => {
     try {
-        
+        const { eventId } = req.params
+
+        await Event.findByIdAndDelete(eventId)
+
+        res 
+            .status(200)
+            .json({ message: "Event deleted Successfully" })
     } catch (error) {
         console.log(error);
         res
@@ -33,7 +56,18 @@ export const deleteEvent = async (req, res) => {
 
 export const registerEvent = async (req, res) => {
     try {
-        
+        const { userId, eventId } = req.params;
+        const { firstName, lastname } = req.body;
+
+        const updatedEvent = await Event.findByIdAndUpdate(
+            eventId,
+            { $push: { registrations: userId } },
+            { new: true }
+        )
+
+        res
+            .status(200)
+            .json({ updatedEvent }) 
     } catch (error) {
         console.log(error);
         res
