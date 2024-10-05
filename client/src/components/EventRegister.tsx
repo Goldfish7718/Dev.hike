dbc1a9127167378f24f7b02fec7aa4fc41aab7e6
Tejas <tejasnanoti2@gmail.com>
2024-10-05 18:34:27 +0530

    refactor: Migrate event creation to use Object Id instead of clerk ID;

--- client/src/components/EventRegister.tsx
+++ client/src/components/EventRegister.tsx
@@ -5,88 +5,105 @@ import {
   DialogTitle,
   DialogTrigger,
   DialogFooter,
-} from "@/components/ui/dialog"
+} from "@/components/ui/dialog";
 
-import { Button } from "./ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { RadioGroup, RadioGroupItem } from "./ui/radio-group"
-import { Plus, UserRoundPlus } from "lucide-react"
-import { useMediaQuery } from "usehooks-ts"
-import { Drawer, DrawerClose, DrawerContent, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from "./ui/drawer"
-import { EventCardProps } from "@/types/types1"
-import { useUser } from "@clerk/clerk-react"
-import { useState } from "react"
-import axios from "axios"
-import { API_URL } from "@/main"
-import { useToast } from "./ui/use-toast"
-import { DialogClose } from "@radix-ui/react-dialog"
+import { Button } from "./ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "./ui/radio-group";
+import { Plus, UserRoundPlus } from "lucide-react";
+import { useMediaQuery } from "usehooks-ts";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "./ui/drawer";
+import { EventCardProps } from "@/types/types1";
+import { useUser as useClerkUser } from "@clerk/clerk-react";
+import { useState } from "react";
+import axios from "axios";
+import { API_URL } from "@/main";
+import { useToast } from "./ui/use-toast";
+import { DialogClose } from "@radix-ui/react-dialog";
+import { useUser } from "@/context/UserContext";
 
 interface EventRegisterProps extends EventCardProps {
   children: React.ReactNode;
-}  
+}
 
-const EventRegisterTrigger = ({ children, title, userRef, _id }: EventRegisterProps) => {
+const EventRegisterTrigger = ({
+  children,
+  title,
+  userRef,
+  _id,
+}: EventRegisterProps) => {
+  const matches = useMediaQuery("(min-width: 768px)");
+  const { user } = useClerkUser();
+  const { currProfile } = useUser();
+  const { toast } = useToast();
 
-  const matches = useMediaQuery('(min-width: 768px)')
-  const { user } = useUser()
-  const { toast } = useToast()
-
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
 
   const setFullname = () => {
-    setFirstName(user?.firstName as string)
-    setLastName(user?.lastName as string)
-  }
+    setFirstName(user?.firstName as string);
+    setLastName(user?.lastName as string);
+  };
 
   const requestRegistration = async () => {
     try {
-      await axios.post(`${API_URL}/events/register/${_id}/${userRef}`, {
-        firstName,
-        lastName
-      })
+      await axios.post(
+        `${API_URL}/events/register/${_id}/${currProfile?._id}`,
+        {
+          firstName,
+          lastName,
+        }
+      );
 
-      window.location.reload()
+      window.location.reload();
     } catch (error) {
       console.log(error);
       toast({
-        title: 'Sorry! An Error occured!',
+        title: "Sorry! An Error occured!",
         duration: 3000,
-        variant: 'destructive'
-      })
+        variant: "destructive",
+      });
     }
-  }
+  };
 
   if (matches)
     return (
       <>
         <Dialog>
-        <DialogTrigger asChild>
-          {children}
-        </DialogTrigger>
+          <DialogTrigger asChild>{children}</DialogTrigger>
           <DialogContent>
-              <DialogHeader>
-                <DialogTitle>Register for {title}</DialogTitle>
-              </DialogHeader>
+            <DialogHeader>
+              <DialogTitle>Register for {title}</DialogTitle>
+            </DialogHeader>
+            <div className="flex flex-col gap-3">
               <div className="flex flex-col gap-3">
-                <div className="flex flex-col gap-3">
-                  <div className="flex flex-col gap-2">
-                    <Label>
-                      First Name:
-                    </Label>
-                    <Input value={firstName} onChange={e => setFirstName(e.target.value)} />
-                  </div>
-
-                  <div className="flex flex-col gap-2">
-                    <Label>
-                      Last Name:  
-                    </Label>
-                    <Input value={lastName} onChange={e => setLastName(e.target.value)} />
-                  </div>
+                <div className="flex flex-col gap-2">
+                  <Label>First Name:</Label>
+                  <Input
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                  />
                 </div>
 
-                {/* <RadioGroup defaultValue="solo" className="flex mt-2 gap-3">
+                <div className="flex flex-col gap-2">
+                  <Label>Last Name:</Label>
+                  <Input
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                  />
+                </div>
+              </div>
+
+              {/* <RadioGroup defaultValue="solo" className="flex mt-2 gap-3">
                   <div className="flex gap-2 items-center">
                     <RadioGroupItem value="solo"/>
                     <Label>Solo</Label>
@@ -104,46 +121,53 @@ const EventRegisterTrigger = ({ children, title, userRef, _id }: EventRegisterPr
                     <Button><Plus size={18} /></Button>
                   </div>
                 </div> */}
-                <Button variant='secondary' onClick={setFullname}>Use my Default name</Button>
-              </div>
-              <DialogFooter>
-                <DialogClose asChild>
-                  <Button onClick={requestRegistration} type="submit" className="px-3 w-full">Register <UserRoundPlus size={16} className="mx-1" /></Button>
-                </DialogClose>
-              </DialogFooter>  
-            </DialogContent>
-          </Dialog>
+              <Button variant="secondary" onClick={setFullname}>
+                Use my Default name
+              </Button>
+            </div>
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button
+                  onClick={requestRegistration}
+                  type="submit"
+                  className="px-3 w-full">
+                  Register <UserRoundPlus size={16} className="mx-1" />
+                </Button>
+              </DialogClose>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
       </>
-    )
+    );
 
   return (
     <>
-    <Drawer>
-      <DrawerTrigger asChild>
-        {children}
-      </DrawerTrigger>
-      <DrawerContent>
-        <DrawerHeader>
-          <DrawerTitle>Register for {title}</DrawerTitle>
-        </DrawerHeader>
-        <div className="flex flex-col gap-3 px-4">
-          <div className="flex flex-col gap-3">
-            <div className="flex flex-col gap-2">
-              <Label>
-                First Name:
-              </Label>
-              <Input value={firstName} onChange={e => setFirstName(e.target.value)} />
+      <Drawer>
+        <DrawerTrigger asChild>{children}</DrawerTrigger>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>Register for {title}</DrawerTitle>
+          </DrawerHeader>
+          <div className="flex flex-col gap-3 px-4">
+            <div className="flex flex-col gap-3">
+              <div className="flex flex-col gap-2">
+                <Label>First Name:</Label>
+                <Input
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+              </div>
+
+              <div className="flex flex-col gap-2">
+                <Label>Last Name:</Label>
+                <Input
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+              </div>
             </div>
 
-            <div className="flex flex-col gap-2">
-              <Label>
-                Last Name:  
-              </Label>
-              <Input value={lastName} onChange={e => setLastName(e.target.value)} />
-            </div>
-          </div>
-
-          {/* <RadioGroup defaultValue="solo" className="flex mt-2 gap-3">
+            {/* <RadioGroup defaultValue="solo" className="flex mt-2 gap-3">
             <div className="flex gap-2 items-center">
               <RadioGroupItem value="solo"/>
               <Label>Solo</Label>
@@ -161,17 +185,24 @@ const EventRegisterTrigger = ({ children, title, userRef, _id }: EventRegisterPr
               <Button><Plus size={18} /></Button>
             </div>
           </div> */}
-          <Button variant='secondary' onClick={setFullname}>Use my Default name</Button>
-        </div>
-        <DrawerFooter>
-          <DrawerClose asChild>
-            <Button onClick={requestRegistration} type="submit" className="px-3 w-full">Register <UserRoundPlus size={16} className="mx-1" /></Button>
-          </DrawerClose>
-        </DrawerFooter>  
-      </DrawerContent>
-    </Drawer>
+            <Button variant="secondary" onClick={setFullname}>
+              Use my Default name
+            </Button>
+          </div>
+          <DrawerFooter>
+            <DrawerClose asChild>
+              <Button
+                onClick={requestRegistration}
+                type="submit"
+                className="px-3 w-full">
+                Register <UserRoundPlus size={16} className="mx-1" />
+              </Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
     </>
-  )
-}
+  );
+};
 
-export default EventRegisterTrigger
+export default EventRegisterTrigger;
--- server/models/eventSchema.js
+++ server/models/eventSchema.js
@@ -1,36 +1,42 @@
-import { model, Schema } from 'mongoose'
+import { model, Schema } from "mongoose";
 
-const eventSchema = new Schema({
+const eventSchema = new Schema(
+  {
     title: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     description: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     organiser: {
-        type: String,
-        reqiured: true,
+      type: String,
+      reqiured: true,
     },
     location: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     registrations: [
-        {
-            userRef: String,
-            firstName: String,
-            lastName: String
-        }
+      {
+        userRef: {
+          type: Schema.Types.ObjectId,
+          ref: "Profile",
+        },
+        firstName: String,
+        lastName: String,
+      },
     ],
     userRef: {
-        type: String
-    }
-},
-{
-    timestamps: true
-}) 
+      type: Schema.Types.ObjectId,
+      ref: "Profile",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const Event = model('Event', eventSchema)
-export default Event
+const Event = model("Event", eventSchema);
+export default Event;
